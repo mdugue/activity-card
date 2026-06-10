dec75a7b1682f66b41f617cfff221b41d70b3c56
Manuel Dugué <mail@manuel.fyi>
2026-06-10 10:06:16 +0200

    review feedback

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,7 +1,8 @@
 import { SpinnerIcon } from "@phosphor-icons/react";
+import type { ComponentProps } from "react";
 import { cn } from "@/lib/utils";
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({ className, ...props }: ComponentProps<"svg">) {
   return (
     <SpinnerIcon
       aria-label="Loading"
